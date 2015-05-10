4a29f60262f94e58c22b56a51e280dbc4e763a1d
Ricardo <ricardo.demauro@gmail.com>
2015-05-10 00:53:35 -0300

    Ajuste relaycommand.

---
+++ ListaDeLeituraApresentacao/ListaDeLeituraApresentacao/ListaDeLeituraApresentacao.Shared/ViewModel/Command/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ListaDeLeituraApresentacao.ViewModel.Command
+{
+    public class RelayCommand : ICommand
+    {
+        public Action<object> ExecuteAction { get; set; }
+
+        public Func<object, bool> CanExecuteFunction { get; set; }
+        
+        public RelayCommand(Action<object> executeAction, Func<object, bool> canExecuteFunction = null)
+        {
+            if (canExecuteFunction == null)
+                canExecuteFunction = (o) => true;
+            if (executeAction == null)
+                throw new ArgumentNullException("executeAction");
+
+            ExecuteAction = executeAction;
+            CanExecuteFunction = canExecuteFunction;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteFunction(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            ExecuteAction(parameter);
+        }
+    }
+}
