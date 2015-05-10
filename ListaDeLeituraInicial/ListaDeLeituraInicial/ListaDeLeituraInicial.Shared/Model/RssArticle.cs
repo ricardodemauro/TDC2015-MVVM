4e1b753f70e68645c586b296f61aacd042b6d1bd
Ricardo <ricardo.demauro@gmail.com>
2015-05-10 00:13:35 -0300

    Ajuste model.

---
+++ ListaDeLeituraInicial/ListaDeLeituraInicial/ListaDeLeituraInicial.Shared/Model/RssArticle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaDeLeituraInicial.Model
+{
+    public class RssArticle
+    {
+        public string Titulo { get; set; }
+
+        public DateTime DataPublicacao { get; set; }
+
+        public string Descricao { get; set; }
+
+        public Uri Link { get; set; }
+
+        public string Thumbnail { get; set; }
+    }
+}
